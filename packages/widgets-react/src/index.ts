4831e9e1c23559b3eab2b768a7509676ded8baab
Damo-Chasey <damienh996@gmail.com>
2024-04-08 14:20:41 +1200

    patch: removed default export

--- packages/widgets-react/src/index.ts
+++ packages/widgets-react/src/index.ts
@@ -1,5 +1,3 @@
-//export { Button } from "~/components/Button";
-
 export { Widget } from "~/components/Widget";
 export { WidgetHeader } from "~/components/WidgetHeader";
 export { WidgetBody } from "~/components/WidgetBody";
